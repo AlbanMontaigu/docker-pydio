441bc1217e60d8ed029ca78cdf761e7c8b297c53
Alban Montaigu <alban.montaigu@gmail.com>
2015-10-03 18:48:24 +0200

    Initial version

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+# ================================================================================================================
+#
+# Pydio with NGINX and PHP-FPM
+#
+# @see https://github.com/AlbanMontaigu/docker-nginx-php-plus
+# @see https://github.com/pydio/pydio-core
+# ================================================================================================================
+
+# Base is a nginx install with php
+FROM amontaigu/nginx-php-plus:5.6.13
+
+# Maintainer
+MAINTAINER alban.montaigu@gmail.com
+
+# Pydio env variables
+ENV PYDIO_VERSION="6.0.8"
+
+# Get Pydio and install it
+RUN mkdir -p --mode=777 /var/backup/pydio \
+    && mkdir -p --mode=777 /usr/src/pydio \
+    && curl -o pydio.tgz -SL https://github.com/pydio/pydio-core/archive/pydio-core-$PYDIO_VERSION.tar.gz \
+    && tar -xzf pydio.tgz --strip-components=3 -C /usr/src/pydio pydio-core-pydio-core-$PYDIO_VERSION/core/src \
+        --exclude=.gitignore \
+        --exclude=.htaccess \
+    && rm pydio.tgz \
+    && chown -Rfv nginx:nginx /usr/src/pydio
+
+# NGINX tuning for pydio
+COPY ./nginx/conf/sites-enabled/default.conf /etc/nginx/sites-enabled/default.conf
+
+# Entrypoint to enable live customization
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+
+# Volume for pydio backup
+VOLUME /var/backup/pydio
+
+# grr, ENTRYPOINT resets CMD now
+ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD ["/usr/bin/supervisord"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# Who and where am I ?
+echo >&2 "[INFO] ---------------------------------------------------"
+echo >&2 "[INFO] GLOBAL INFORMATIONS"
+echo >&2 "[INFO] ---------------------------------------------------"
+echo >&2 "[INFO] whoami : $(whoami)"
+echo >&2 "[INFO] pwd : $(pwd)"
+
+# Backup the prev install in case of fail...
+echo >&2 "[INFO] ---------------------------------------------------"
+echo >&2 "[INFO] Backup old pydio installation in $(pwd)"
+echo >&2 "[INFO] ---------------------------------------------------"
+tar -zcvf /var/backup/pydio/pydio-v$(date '+%Y%m%d%H%M%S').tar.gz .
+echo >&2 "[INFO] Complete! Backup successfully done in $(pwd)"
+
+# Since pydio can be upgraded by overwriting files do the upgrade !
+# @see https://github.com/thibaud-rohmer/PhotoShow/
+# 
+# File copy strategy taken from wordpress entrypoint
+# @see https://github.com/docker-library/wordpress/blob/master/fpm/docker-entrypoint.sh
+echo >&2 "[INFO] ---------------------------------------------------"
+echo >&2 "[INFO] Installing or upgrading pydio in $(pwd)"
+echo >&2 "[INFO] ---------------------------------------------------"
+echo >&2 "[INFO] Removing old installation"
+find -maxdepth 1 ! -regex '^\./data.*$' ! -regex '^\.$' -exec rm -rvf {} +
+echo >&2 "[INFO] Extracting new installation"
+tar cvf - --one-file-system -C /usr/src/pydio . | tar xvf -
+echo >&2 "[INFO] Fixing rights"
+chown -Rfv nginx:nginx .
+echo >&2 "[INFO] Complete! PhotoShow has been successfully installed / upgraded to $(pwd)"
+
+# Exec main command
+exec "$@"
